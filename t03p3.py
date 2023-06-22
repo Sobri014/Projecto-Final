b73fe52b46a81fd47af1c2813961a9bdfbafe9a3
csobrinom <c.sobrinom@alumnos.upm.es>
2023-06-22 11:40:02 +0200

    Nuevo Proyecto Final curso python

--- t03p3.py
+++
@@ -1,6 +0,0 @@
-lista = ['Carlos', 'Sobrino Muñoz', '14/11/1998', 168, 36.5, 'ºC']
-print(type(lista))
-print(lista)
-lista[4] = 36.5 + 0.5
-print("Lista modificada:")
-print(lista)
--- t07p2.py
+++
@@ -1,9 +0,0 @@
-#diccionarios
-diccionario = {
-    "1":"Hola",
-    "2":"Mundo",
-    "3":"Adios"
-}
-
-hola = diccionario.get(1, "hola")
-print(hola)
